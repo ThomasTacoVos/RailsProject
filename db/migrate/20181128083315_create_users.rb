37581642557872d01db2f0d25acc881f04e29878
Thomas Vos <tvvos@hotmail.nl>
2018-11-28 14:28:54 +0100

    Migration fix

--- db/migrate/20181128083315_create_users.rb
+++ db/migrate/20181128083315_create_users.rb
@@ -4,7 +4,7 @@ class CreateUsers < ActiveRecord::Migration[5.2]
       #   turn off auto id for this migration         , :id => false
       t.column "first_name", :string, :limit => 25
       t.string "last_name", :limit => 50
-      t.string "email", :default => '', null => false
+      t.string "email", :default => '', :null => false
       t.string "password", :limit => 40
 
       t.timestamps
